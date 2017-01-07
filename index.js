9d3e11e9a3c56e21a191b5c95e228b51017fea6b
Claire Martinez <cmartinez@quickbase.com>
2017-01-06 22:35:31 -0500

    more rewire injected lines to ignore

--- index.js
+++ index.js
@@ -2,17 +2,24 @@
 module.exports = function({types: t}) {
     var commentToIgnoreCoverage = " istanbul ignore next ";
     var methodsToIgnoreRegEx =
-        /_get__|_get_original__|_assign__|_set_original__|_update_operation__|_set__|_reset__|_with__/;
-    function addIgnoreComment(context) {
-        context.addComment("leading", commentToIgnoreCoverage, true);
+        /_get__|_get_original__|_assign__|_set_original__|_update_operation__|_set__|_reset__|_with__|_filterWildcardImport__|addNonEnumerableProperty/;
+
+    function addIgnoreComment(context, addBefore) {
+        var where = addBefore ? "leading" : "trailing" ;
+        context.addComment(where, commentToIgnoreCoverage, true);
     }
+
     return {
         visitor: {
             Function(path) {
                 // add istanbul ignore on the the rewire injected function statements
-                if (path.node.id && path.node.id.name &&
-                    methodsToIgnoreRegEx.exec(path.node.id.name) !== null) {
-                    addIgnoreComment(path);
+                if (path.node.id && path.node.id.name) {
+                    if (methodsToIgnoreRegEx.exec(path.node.id.name) !== null) {
+                        addIgnoreComment(path, true);
+                    }
+                    if (path.node.id.name === 'addNonEnumerableProperty') {
+                        addIgnoreComment(path, false);
+                    }
                 }
             }
         }
